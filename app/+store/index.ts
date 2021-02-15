57f2bc09903c947f77a2466d6d4f2016be3096c0
Maria Raykova <66566569+MariaRaykova@users.noreply.github.com>
2021-02-15 14:11:17 +0200

    Delete app directory

--- app/+store/index.ts
+++
@@ -1,9 +0,0 @@
-import { ActionReducerMap } from "@ngrx/store";
-import { authReducer, IAuthState } from "./reducers";
-
-export interface IRootState {
-    readonly auth: IAuthState;
-}
-export const reducers: ActionReducerMap<IRootState> = {
-    auth: authReducer
-}
